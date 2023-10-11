1e29c669aa1d480085d0fdf9f9d263d0ee902ef8
xSantiiiiiiiiiiiix <xSantiiiiiiiiiiiix>
2023-10-11 11:35:12 -0300

    11_10

---
+++ Waigel Santiago/Presupuesto/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Presupuesto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxProductos = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBoxProductos = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(213, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Calcular";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxProductos
+            // 
+            this.textBoxProductos.Location = new System.Drawing.Point(213, 12);
+            this.textBoxProductos.Name = "textBoxProductos";
+            this.textBoxProductos.Size = new System.Drawing.Size(100, 20);
+            this.textBoxProductos.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(138, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Presupuesto:";
+            // 
+            // listBoxProductos
+            // 
+            this.listBoxProductos.FormattingEnabled = true;
+            this.listBoxProductos.Location = new System.Drawing.Point(12, 12);
+            this.listBoxProductos.Name = "listBoxProductos";
+            this.listBoxProductos.Size = new System.Drawing.Size(120, 95);
+            this.listBoxProductos.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(480, 349);
+            this.Controls.Add(this.listBoxProductos);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxProductos);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxProductos;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBoxProductos;
+    }
+}
+
---
+++ Waigel Santiago/Presupuesto/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections.Generic;
+
+
+namespace Presupuesto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Leer el contenido del archivo de precios
+            string[] lineas = File.ReadAllLines("precios.txt");
+            Dictionary<string, double> precios = new Dictionary<string, double>();
+
+            foreach (string linea in lineas)
+            {
+                string[] partes = linea.Split(':');
+                if (partes.Length == 2)
+                {
+                    string producto = partes[0].Trim();
+                    if (!precios.ContainsKey(producto))
+                    {
+                        double precio;
+                        if (double.TryParse(partes[1], out precio))
+                        {
+                            precios.Add(producto, precio);
+                        }
+                    }
+                }
+            }
+
+            // Calcular el presupuesto
+            string[] productosSeleccionados = textBoxProductos.Text.Split(new char[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+            double presupuestoTotal = 0;
+
+            foreach (string productoSeleccionado in productosSeleccionados)
+            {
+                string producto = productoSeleccionado.Trim();
+                if (precios.ContainsKey(producto))
+                {
+                    presupuestoTotal += precios[producto];
+                }
+            }
+
+            // Mostrar el presupuesto total
+            label1.Text = "Presupuesto Total: $" + presupuestoTotal.ToString("0.00");
+        }
+    }
+}
